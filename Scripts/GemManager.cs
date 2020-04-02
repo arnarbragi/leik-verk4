d41ca263ebf7eef8ef760ba4ee2e0d0568db07d5
arnarbragi <42640855+arnarbragi@users.noreply.github.com>
2020-04-02 19:52:56 +0000

    Mitt stuff
    gat ekki sett inn tiles út af fjölda skráa

---
+++ Scripts/GemManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemManager : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            Destroy(other.gameObject);
+            ScoreManager.instance.ChangeScore();
+        }
+    }
+}
